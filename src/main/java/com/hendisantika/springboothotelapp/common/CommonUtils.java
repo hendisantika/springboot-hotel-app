51c4718ec3e2a5361dfe6715ebd2c0e71b9accc2
Hendi Santika <hendisantika@gmail.com>
2020-02-12 07:14:10 +0700

    #4 - Add CommonUtils

---
+++ src/main/java/com/hendisantika/springboothotelapp/common/CommonUtils.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springboothotelapp.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-hotel-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/02/20
+ * Time: 07.13
+ */
+@Component
+public class CommonUtils implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext = null;
+}
