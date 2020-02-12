441d221e081896039b8f325b38b79ada42e2caac
Hendi Santika <hendisantika@gmail.com>
2020-02-12 07:14:26 +0700

    #5 - Add setApplicationContext in CommonUtils

--- src/main/java/com/hendisantika/springboothotelapp/common/CommonUtils.java
+++ src/main/java/com/hendisantika/springboothotelapp/common/CommonUtils.java
@@ -1,5 +1,6 @@
 package com.hendisantika.springboothotelapp.common;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
@@ -17,4 +18,12 @@ import org.springframework.stereotype.Component;
 public class CommonUtils implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext = null;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (CommonUtils.applicationContext == null) {
+            CommonUtils.applicationContext = applicationContext;
+        }
+    }
+
 }
