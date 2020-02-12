66fb1f5cba839072b3497f0358063b83c931204a
Hendi Santika <hendisantika@gmail.com>
2020-02-12 07:09:51 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/springboothotelapp/SpringbootHotelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboothotelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHotelAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
