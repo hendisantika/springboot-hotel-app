7ac26274a7a3842b0237fd38bebe089d4bbc3fbd
Hendi Santika <hendisantika@gmail.com>
2020-02-13 06:21:56 +0700

    #35 - Add HomeController

---
+++ src/main/java/com/hendisantika/springboothotelapp/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springboothotelapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-hotel-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/02/20
+ * Time: 06.21
+ */
+@Controller
+public class HomeController {
+}
