246b2f22a7c356af961162f87d36ab5b7469c8cd
Hendi Santika <hendisantika@gmail.com>
2020-02-13 06:11:54 +0700

    #30 - Add getCityAvailability in WebserviceManager Service

--- src/main/java/com/hendisantika/springboothotelapp/service/WebserviceManager.java
+++ src/main/java/com/hendisantika/springboothotelapp/service/WebserviceManager.java
@@ -1,13 +1,22 @@
 package com.hendisantika.springboothotelapp.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hendisantika.springboothotelapp.entity.Destination;
 import com.hendisantika.springboothotelapp.entity.ExchangeRate;
+import com.hendisantika.springboothotelapp.entity.HotelAvailability;
 import com.hendisantika.springboothotelapp.entity.Nationality;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -75,5 +84,45 @@ public class WebserviceManager {
         ResponseEntity<Destination[]> resp = restTemplate.getForEntity(svcProperty.getRootUrl() + "/suggestion" +
                 "/destination?key=" + key, Destination[].class);
         return resp.getBody();
+    }
+
+    /**
+     * fetch hotel availabilities
+     *
+     * @param param   json
+     * @param isHotel true if destination is hotel
+     * @return
+     */
+    public HotelAvailability[] getCityAvailability(JSONObject param, boolean isHotel) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<String>(param.toString(), headers);
+
+        // determine url
+        String strUrl = "/hotel/availability/city";
+        if (isHotel) {
+            strUrl = "/hotel/availability/hotel";
+        }
+
+        // call available web service
+        ResponseEntity<JSONObject> response = restTemplate.exchange(svcProperty.getRootUrl() + strUrl,
+                HttpMethod.POST, entity, JSONObject.class);
+        JSONObject jsonResponse = response.getBody();
+        JSONArray jsonArray = jsonResponse.getJSONArray("hotelAvailabilities");
+
+        //
+        // transfer json to object
+        //
+        HotelAvailability[] hotelAvailabilities = null;
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            hotelAvailabilities = mapper.readValue(jsonArray.toString(), HotelAvailability[].class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return hotelAvailabilities;
     }
 }
