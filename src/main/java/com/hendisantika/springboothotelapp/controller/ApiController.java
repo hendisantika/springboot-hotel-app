05b419852322a6a05920f5544f80957cde0de59f
Hendi Santika <hendisantika@gmail.com>
2020-02-13 06:20:52 +0700

    #32 - Add ApiController

---
+++ src/main/java/com/hendisantika/springboothotelapp/controller/ApiController.java
@@ -0,0 +1,18 @@
+package com.hendisantika.springboothotelapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-hotel-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/02/20
+ * Time: 06.20
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiController extends BaseController {
+}
