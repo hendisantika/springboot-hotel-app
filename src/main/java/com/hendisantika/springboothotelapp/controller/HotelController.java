ad3b1ba2b32883163008dcee02ff359170fd28de
Hendi Santika <hendisantika@gmail.com>
2020-02-14 06:12:42 +0700

    #43 - Add hotel API in HotelController

--- src/main/java/com/hendisantika/springboothotelapp/controller/HotelController.java
+++ src/main/java/com/hendisantika/springboothotelapp/controller/HotelController.java
@@ -3,7 +3,11 @@ package com.hendisantika.springboothotelapp.controller;
 import com.hendisantika.springboothotelapp.service.WebserviceManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +28,12 @@ public class HotelController extends BaseController {
 
     @Autowired
     private WebserviceManager apiManager;
+
+    @GetMapping(value = "/detail/{id}")
+    public String hotel(@PathVariable("id") int id, Map<String, Object> model) {
+        model.put("page", "index");
+
+        return "hotel";
+    }
+
 }
